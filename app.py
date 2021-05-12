53a5ecf7efd2207f6087db352ac1fd4547138a34
leandermaerkisch <l.maerkisch@gmail.com>
2021-05-12 14:39:48 +0200

    --display uploaded image,  added loading sequence

--- app.py
+++ app.py
@@ -1,14 +1,48 @@
+import os
+from fastai import *
+from fastai.vision import *
+
+import numpy as np
+import pickle
+
 from flask import Flask, render_template, url_for, request, redirect, jsonify
 from werkzeug.utils import secure_filename
-import pickle
+import time
+
 
 
 app = Flask(__name__)
-model = "Model Example" #pickle.load(open('model.pkl', 'rb'))
+UPLOAD_FOLDER = "/Users/leandermarkisch/Documents/FlaskProject/static/images"
+MODEL = None
+DEVICE = "cpu"
+FILE = None
+#with open('export.pkl', 'rb') as f:
+#    MODEL = pickle.load(f)
+
+def predict(image_path, model):
+    mean = (0.485, 0.456, 0.406)
+    std = (0.229, 0.224, 0.225)
+    test_images = [image_path]
+    test_targets = [0]
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    '''
+    For rendering results on HTML GUI
+    '''
+    output = "Pneunomia" #round(prediction[0], 2)
+    if request.method == "POST":
+        image_file = request.files["image"]
+        if image_file:
+            filename = secure_filename(image_file.filename)
+            FILE  = filename
+            image_location = os.path.join(
+                UPLOAD_FOLDER,
+                filename
+            )
+            image_file.save(image_location)
+            return render_template('index.html', uploaded_img="static/images/"+filename)
     return render_template('index.html')
 
 @app.route('/predict',methods=['POST'])
@@ -16,25 +50,14 @@ def predict():
     '''
     For rendering results on HTML GUI
     '''
-    #int_features = [int(x) for x in request.form.values()]
-    #final_features = [np.array(int_features)]
-    #prediction = model.predict(final_features)
+    output = "Pneunomia"
+    pred = "80"
+    pred_img = "mri_annotated.png"
+    time.sleep(5)
+    return render_template('index.html', prediction_text='You have {} with {}% probability'.format(output, pred), uploaded_img="static/images/mri_raw.jpg", annotated_img="static/images/"+pred_img)
 
-    output = "Pneumonia" #round(prediction[0], 2)
-    og_image = "https://prod-images-static.radiopaedia.org/images/157210/332aa0c67cb2e035e372c7cb3ceca2_jumbo.jpg"
 
-    return render_template('index.html', prediction_text='You have {} with 85% probability'.format(output), img_src = og_image)
-
-@app.route('/predict_api',methods=['POST'])
-def predict_api():
-    '''
-    For direct API calls trought request
-    '''
-    data = request.get_json(force=True)
-    prediction = model.predict([np.array(list(data.values()))])
-
-    output = prediction[0]
-    return jsonify(output)
 
 if __name__ == "__main__":
+    #MODEL.to(DEVICE)
     app.run(port=12000, debug=True)
